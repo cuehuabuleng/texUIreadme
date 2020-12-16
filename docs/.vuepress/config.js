4860d5cdd7099c0e310359d8e102831624c4b055
cuehuabuleng <1079713849@qq.com>
2020-12-16 21:01:56 +0800

    feat:完成首页以及图标的配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title:'tex-ui组件库的使用文档',
+    description:'基于vue3.0开发',
+    head:[
+        ['link', {rel:'icon', href:'/01.ico'}]
+    ],
+    themeConfig:{
+        logo:'/avatar.jpg',
+        nav:[
+            { text: '首页', link: '/', icon: 'reco-home' },
+            { text: '博客网站', link: 'http://114.55.93.74:8014/', icon: 'reco-blog' },
+            { text: 'github', link: 'https://github.com/cuehuabuleng/tex-ui', icon: 'reco-github' }
+        ]
+    }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-# Hello VuePress
+---
+home: true
+heroImage: /02.png
+heroText: tex-ui组件库使用文档
+tagline: 东方根号三
+actionText: 快速开始 →
+actionLink: /pages/button/
+features:
+- title: 墨菲定律
+  details: 如果有两种或者两种以上的方式去做某件事情，而其中一种选择方式将导致灾难，则必定有人会做出这种选择————爱德华.墨菲
+- title: 波克定理
+  details: 只有在争论中，才可能诞生最好的结果。————詹姆斯.波克
+- title: 托利得定理
+  details: 测验一个人的智力是否属于上乘，只看脑子里能否同时容纳两种相反的思想而无碍于其处世行事。
+footer: MIT Licensed | Copyright © 2020-present 东方根号三
+---
