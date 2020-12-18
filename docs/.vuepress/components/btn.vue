ff9522b631553618caba1ed99bd3cebd850de8d0
cuehuabuleng <1079713849@qq.com>
2020-12-18 22:32:35 +0800

    pending:在md文件中使用hightline.js库实现代码高亮

---
+++ docs/.vuepress/components/btn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <tex-button>默认按钮</tex-button>
+    <tex-button type="primary">主要按钮</tex-button>
+    <tex-button type="success">成功按钮</tex-button>
+    <tex-button type="info">信息按钮</tex-button>
+    <tex-button type="warning">警告按钮</tex-button>
+    <tex-button type="danger">危险按钮</tex-button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {},
+};
+</script>
